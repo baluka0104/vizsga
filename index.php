f703bf0f846884caf759b12f366aa8ae2b247a98
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-09-20 22:41:27 +0200

    Update index.php
    add logo to header

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	
 		<div class="main-nav">
 		    <h1 class="logo">
-                <!-- <img src="logo.png" alt="salty"> -->
+                <img src="logo.png" alt="salty">
                 <a>Salty</a>
             </h1>
 			<a class="nav-item" href="./?p=rec">Receptek</a>
