a9cd2cbfb057af7e8ccc7672163c1abb430f37d6
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-09-27 17:24:24 +0200

    Update receptek.php

--- receptek.php
+++ receptek.php
@@ -1,1 +1,25 @@
-<h2>Receptek</h2>
+<h2>Receptek</h2> 
+
+
+<div>
+    <form class="recept-element" method="post" action="recept-connect.php">
+        <div class="recept-item">
+            <label class="receptName">Recept neve</label>
+            <div class="recept">maga a recept</div>
+        </div>
+
+
+
+
+        
+    </form>
+</div>
+
+
+
+
+<?php
+
+
+
+?>
