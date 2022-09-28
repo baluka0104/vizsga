e7aa76c0d4ab83aab7ce58308a4831ace2da270a
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-09-28 02:08:50 +0200

    recfel helyére tevése

--- recfel.php
+++ recfel.php
@@ -1,13 +1,27 @@
 <h2>Receptfeltöltés</h2>
 
 <?php
-if( !isset($_SESSION['uid']) ) print("Előbb be kell jelentkezned!");
+if( !isset($_SESSION['uid']) ) print("Előbb be kell jelentkezned!"); 
 else
 {
-
+    echo '<div>
+        <form class="recept-element" method="post" action="recept-connect.php">
+            <div class="recept-item">
+                <input type="text" class="receptName" name="receptName" value="Recept neve"></label><br>
+                <textarea class="osszetevok" name="osszetevok">maga a recept</textarea><br>
+                <textarea class="elkeszites" name="elkeszites">Elkészítés</textarea>
+                <div class="form-element">
+                        <button type="submit" name="recSubmit">Receptek</button>
+                </div>
+            </div>
+        
+            
+        </form>
+    </div>';
 
 }
 
 
 
 ?>
+
