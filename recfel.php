d654e9502902558925112035aceffe9e2f0d948d
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-09-28 02:28:29 +0200

    recfel formázás

--- recfel.php
+++ recfel.php
@@ -7,11 +7,11 @@ else
     echo '<div>
         <form class="recept-element" method="post" action="recept-connect.php">
             <div class="recept-item">
-                <input type="text" class="receptName" name="receptName" value="Recept neve"></label><br>
-                <textarea class="osszetevok" name="osszetevok">maga a recept</textarea><br>
-                <textarea class="elkeszites" name="elkeszites">Elkészítés</textarea>
+                <input type="text" class="receptName" name="receptName" placeholder="Név"></label><br>
+                <textarea class="osszetevok" name="osszetevok" placeholder="Ide írd a hozzávalókat" cols="70" rows="12"></textarea><br>
+                <textarea class="elkeszites" name="elkeszites" placeholder="Ide írd az elkészítés lépéseit" cols="70" rows="12"></textarea>
                 <div class="form-element">
-                        <button type="submit" name="recSubmit">Receptek</button>
+                        <button type="submit" name="recSubmit">Recept beküldése</button>
                 </div>
             </div>
         
