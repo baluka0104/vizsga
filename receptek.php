858313295a33e0d27c9cf66bfd87cdc02c7d8df4
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-10-03 23:58:08 +0200

    Dokumentáció, receptek update

--- receptek.php
+++ receptek.php
@@ -2,16 +2,9 @@
 
 <?php
 
-
-
-
-
-
-
     /* Kapcsolódás az adatbázishoz */
 
     $adb = mysqli_connect( "localhost", "root", "", "vizsga" );
-
 
 
     /* Receptek lekérdezése */
@@ -25,23 +18,29 @@
     " ) ;
 
 
-
     echo "<div id='etelek'>" ;
         while( $sor = mysqli_fetch_array( $tabla ) )
         {
+            // echo "<div style=' border: solid 1px black; margin: 5px;'>
+            //     <div class='recept-nev' style=' font-size: 25px; '>$sor[rnev]</div><br>
+            //     <div class='recept-osszetevok'>
+            //     <ul>";
+            
+            //         var_dump(explode(";", $sor['osszetevok']));
+            //         var_dump(count($sor['osszetevok']));
+            //         for($i = 0; $i < count($sor['osszetevok']); $i++){
+            //             echo "<li>". $sor['osszetevok'][$i] ."</li>";
+            //         };
+                    
+            //     echo "</ul>
+            //     </div><br>
+            //     <div class='recept-elkeszites'>$sor[elkeszites]</div><br>
+            //     <br><br>
+            // </div>";
+
             echo "<div style=' border: solid 1px black; margin: 5px;'>
                 <div class='recept-nev' style=' font-size: 25px; '>$sor[rnev]</div><br>
-                <div class='recept-osszetevok'>
-                <ul>";
-            
-                    var_dump(explode(";", $sor['osszetevok']));
-                    var_dump(count($sor['osszetevok']));
-                    for($i = 0; $i < count($sor['osszetevok']); $i++){
-                        echo "<li>". $sor['osszetevok'][$i] ."</li>";
-                    };
-                    
-                echo "</ul>
-                </div><br>
+                <div class='recept-osszetevok'>$sor[osszetevok]</div><br>
                 <div class='recept-elkeszites'>$sor[elkeszites]</div><br>
                 <br><br>
             </div>";
