abccd0b53373a9b0c9d74e2a5105c875bbb10aed
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-10-04 17:45:36 +0200

    Create fav.js

---
+++ fav.js
@@ -0,0 +1,11 @@
+//       Like Button   
+var btnvar1 = document.getElementById('btnh1');
+
+function Toggle1(){
+         if (btnvar1.style.color =="red") {
+             btnvar1.style.color = "grey"
+         }
+         else{
+             btnvar1.style.color = "red"
+         }
+}
