236264825aa8e789a9ef33b15400cf7d52dfa815
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-10-04 16:45:47 +0200

    recfel update

--- recfel-connect.php
+++ recfel-connect.php
@@ -5,19 +5,22 @@
 
     if (isset($_POST['recSubmit'])) { 
         $conn = new mysqli('localhost', 'root', '', 'vizsga');
-    if($conn->connect_error){
-        echo "$conn->connect_error";
-        die("Connection Failed : ". $conn->connect_error);
-    }
-    $stmt = $conn->prepare("insert into receptek(rnev, osszetevok, elkeszites) values(?, ?, ?)");
-    $stmt->bind_param("sss", $receptname, $osszetevok, $elkeszites);
+        if($conn->connect_error){
+            echo "$conn->connect_error";
+            die("Connection Failed : ". $conn->connect_error);
+        }
+        $stmt = $conn->prepare("insert into receptek(rnev, osszetevok, elkeszites) values(?, ?, ?)");
+        $stmt->bind_param("sss", $receptname, $osszetevok, $elkeszites);
 
-
-    echo $receptname;
-
-    $stmt -> close();
-    $conn -> close();
-
+        if (!$execval = $stmt->execute()) {
+            echo "error";
+            exit();
+        } else {
+            $stmt->close();
+            $conn->close();
+            header("location: index.php?p=rec&recfel=successfull");
+            exit();
+        }
     }
     
 ?>
--- recfel.php
+++ recfel.php
@@ -5,10 +5,10 @@ if( !isset($_SESSION['uid']) ) print("Előbb be kell jelentkezned!");
 else
 {
     echo '<div>
-        <form class="recept-element" method="post" action="recept-connect.php">
+        <form class="recept-element" method="post" action="recfel-connect.php">
             <div class="recept-item">
                 <input type="text" class="receptName" name="receptName" placeholder="Név"></label><br>
-                <textarea class="osszetevok" name="osszetevok" placeholder="Ide írd a hozzávalókat" cols="70" rows="12"></textarea><br>
+                <textarea class="osszetevok" name="osszetevok" placeholder="Ide írd a hozzávalókat (;) jellel elválasztva" cols="70" rows="12"></textarea><br>
                 <textarea class="elkeszites" name="elkeszites" placeholder="Ide írd az elkészítés lépéseit" cols="70" rows="12"></textarea>
                 <div class="form-element">
                         <button type="submit" name="recSubmit">Recept beküldése</button>
