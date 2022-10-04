1dba0f9a8b6e1a338e773b1722d7264de2424485
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-10-04 19:55:22 +0200

    recfel minimum required

--- recfel-connect.php
+++ recfel-connect.php
@@ -9,6 +9,22 @@
             echo "$conn->connect_error";
             die("Connection Failed : ". $conn->connect_error);
         }
+        if (strlen($receptName) < 4) {
+            header("location: ../vizsga/index.php?p=recfel&error=receptName-min-4");
+            
+            if (strlen($osszetevok) < 15) {
+            header("location: ../vizsga/index.php?p=recfel&error=hozzavalok-min-15");
+
+                if (strlen($elkeszites) < 15) {
+                header("location: ../vizsga/index.php?p=recfel&error=elkeszites-min-15");
+                exit();
+                }
+            }
+        }
+        
+        
+
+
         $stmt = $conn->prepare("insert into receptek(rnev, osszetevok, elkeszites) values(?, ?, ?)");
         $stmt->bind_param("sss", $receptname, $osszetevok, $elkeszites);
 
--- recfel.php
+++ recfel.php
@@ -1,4 +1,4 @@
-<h2>Receptfeltöltés</h2>
+<h2 class="recfelTitle">Receptfeltöltés</h2>
 
 <?php
 if( !isset($_SESSION['uid']) ) print("Előbb be kell jelentkezned!"); 
@@ -7,9 +7,9 @@ else
     echo '<div>
         <form class="recept-element" method="post" action="recfel-connect.php">
             <div class="recept-item">
-                <input type="text" class="receptName" name="receptName" placeholder="Név"></label><br>
-                <textarea class="osszetevok" name="osszetevok" placeholder="Ide írd a hozzávalókat (;) jellel elválasztva" cols="70" rows="12"></textarea><br>
-                <textarea class="elkeszites" name="elkeszites" placeholder="Ide írd az elkészítés lépéseit" cols="70" rows="12"></textarea>
+                <input type="text" class="receptName" name="receptName" required placeholder="Név"></label><br>
+                <textarea class="osszetevok" name="osszetevok" required placeholder="Ide írd a hozzávalókat (;) jellel elválasztva" cols="70" rows="12"></textarea><br>
+                <textarea class="elkeszites" name="elkeszites" required placeholder="Ide írd az elkészítés lépéseit" cols="70" rows="12"></textarea>
                 <div class="form-element">
                         <button type="submit" name="recSubmit">Recept beküldése</button>
                 </div>
