a9894a41f7b68473c5b030becc7f5d6337e1d5bf
Balázs <90355094+baluka0104@users.noreply.github.com>
2022-10-07 02:03:23 +0200

    js probalkozas

--- fav.js
+++ fav.js
@@ -1,13 +1,50 @@
 //       Like Button   
 
-    var btnvar1 = document.getElementsByClassName("btn");
 
-    function Toggle(){
-            if (btnvar1.style.color == "#ff0000") 
-            {
-                btnvar1.style.color = "grey";
-            }
-            else{
-                btnvar1.style.color = "red";
-            }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//3. proba
+
+// $(document).ready(function() {
+
+//     $('.fa-heart').on('click', function() {
+//         var post_id = $(this).data('id');
+//         alert(post_id);
+//     });
+// });
+
+
+
+//2.proba
+    // const button = document.querySelector(".btns .fa-heart");
+
+
+    // button.onclick = ()=> {
+    //     let xhr = new XMLHttpRequest();
+    //     xhr.open("GET", "../localhost/vizsga/index.php?p=<?php echo $_SESSION['uid']; ?>", true);
+    //     xhr.onload = ()=> {
+    //         if(xhr.readyState === XMLHttpRequest.DONE) {
+    //             if(xhr.status === 200) {
+    //                 button.classList.toggle("active");
+    //             }
+    //         }
+    //     }
+    //     xhr.send();
+    // }
